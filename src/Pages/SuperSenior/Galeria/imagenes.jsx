75565e96c7266307470ef0953120d46abb3da0ae
Sebastian Pujol <s.pujol1211@gmail.com>
2024-03-28 03:16:41 -0300

    cambios

--- src/Pages/SuperSenior/Galeria/imagenes.jsx
+++ src/Pages/SuperSenior/Galeria/imagenes.jsx
@@ -1,28 +1,32 @@
-import * as React            from 'react';
-import { Grid }              from "@mui/material";
+import * as React from 'react';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
 
-//imagenes
-import imagen1                from '../../../imagenes/Seccion/Messi.jpg';
-import imagen2                from '../../../imagenes/Seccion/alexis.webp';
-import imagen3                from '../../../imagenes/Seccion/mago.jpg';
-import imagen4                from '../../../imagenes/Seccion/puyol.jpg';
-import imagen5                from '../../../imagenes/Seccion/ronaldino.jpg';
+// Import your local images directly
+import imagen1 from '../../../imagenes/Seccion/Messi.jpg';
+import imagen2 from '../../../imagenes/Seccion/alexis.webp';
+import imagen3 from '../../../imagenes/Seccion/mago.jpg';
+import imagen4 from '../../../imagenes/Seccion/puyol.jpg';
+import imagen5 from '../../../imagenes/Seccion/ronaldino.jpg';
 
-export default function CollapsibleTable() {
-  
+const images = [imagen1, imagen2, imagen3, imagen4, imagen5]; // Array of local image paths
+
+export default function TitlebarBelowImageList() {
   return (
-    <Grid>
-      <Grid className="grisCuadro">
-        <h2 style={{ textAlign: 'center' }}>Imagenes del campeonato</h2>
-          <section>
-            <img src={imagen1} alt=""></img>
-            <img src={imagen2} alt=""></img>
-            <img src={imagen3} alt=""></img>
-            <img src={imagen4} alt=""></img>
-            <img src={imagen5} alt=""></img>
-          </section>
-          
-      </Grid>
-    </Grid>
+    <ImageList sx={{ width: 'auto', height: 500, columnCount: 2 }}>  {/* Set fixed width, height and columnCount to 2 */}
+      {images.map((image, index) => (
+        <ImageListItem key={index}>
+          <img
+            src={image}
+            alt={`Image ${index + 1}`}
+            loading="lazy"
+          />
+          <ImageListItemBar
+            position="below"
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
   );
 }
